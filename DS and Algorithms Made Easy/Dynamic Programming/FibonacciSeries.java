9d0b6e42370035e8745cf6b1df0f1c6bb5b912de
Tushar Kharbanda <tushar.kharbanda72@gmail.com>
2016-12-06 22:47:42 +0530

    fibonacci using dp

--- DS and Algorithms Made Easy/Dynamic Programming/FibonacciSeries.java
+++ DS and Algorithms Made Easy/Dynamic Programming/FibonacciSeries.java
@@ -1,13 +1,14 @@
+import java.util.*;
 class FibonacciSeries{
-		public static int fibonacci(int n){
-			if(n == 0) return 0;
-			if(n == 1) return 1;
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
+	public static int fibonacci(int n){
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
 
-		public static void main(String[] args){
-				Scanner in = new Scanner(System.in);
-				int n = in.nextInt();
-				System.out.println(fibonacci(n));	
-		}
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(fibonacci(n));	
+	}
 }
---
+++ DS and Algorithms Made Easy/Dynamic Programming/FibonacciSeriesDp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class FibonacciSeriesDp{
+	public static int fibonacci(int n, int[] dp){
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		if(dp[n] != 0) return dp[n];
+		return dp[n] = (fibonacci(n-1, dp) + fibonacci(n-2, dp));
+	}
+
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] dp = new int[n+1];
+		System.out.println(fibonacci(n, dp));	
+	}
+}
